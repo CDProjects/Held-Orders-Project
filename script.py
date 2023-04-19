8cc6603633c5ebb77d33648d501663f2991c9497
Kiwicase <caseydentprojects@gmail.com>
2023-04-19 12:44:45 +0300

    Scope added

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 key_path = 'C:/Projects/Held Orders Project/held-orders-project-1f20881965f6.json'
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
