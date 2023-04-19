e46807bd010f0ddb689f309ff7b4fd460ddd5afd
Kiwicase <caseydentprojects@gmail.com>
2023-04-19 12:51:49 +0300

    Scope and credentials added

--- script.py
+++ script.py
@@ -4,3 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 key_path = 'C:/Projects/Held Orders Project/held-orders-project-1f20881965f6.json'
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
+
+scope_str = ' '.join(scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, scope_str)
+
